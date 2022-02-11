10eea7dc1060f08130e507ee67238d163b20eca0
Valentin Castravete <valentincastravete@gmail.com>
2022-02-11 19:29:14 +0100

    Aavance DWES

--- logrocho/view/backend/js/bares.js
+++ logrocho/view/backend/js/bares.js
@@ -2,8 +2,8 @@
 // https://github.com/johndatserakis/file-upload-with-preview/tree/master
 
 let ajax = new AjaxSerialization();
-let num_filas = 2;
-let pagina = 0;
+let registros = 5;
+let pag = 0;
 let order_by = 0;
 let order_by_asc = 1;
 let departamentos;
@@ -11,71 +11,94 @@ let departamentos;
 let cantidad = document.getElementById("basic-datatable_length").selected.value;
 let pagina = document.getElementById("alternative-page-datatable_paginate").querySelector(".active").value;
 
+
+let ths = `
+        <th style="cursor: pointer;" class="w-5" onclick="ordenar(0);">ID ↓</th>
+        <th style="cursor: pointer;" onclick="ordenar(1);">Nombre</th>
+        <th style="cursor: pointer;" onclick="ordenar(2);">Dirección</th>
+        <th style="cursor: pointer;" onclick="ordenar(3);">Terraza</th>
+        <th style="cursor: pointer;" onclick="ordenar(4);">Latitud</th>
+        <th style="cursor: pointer;" onclick="ordenar(5);">Longitud</th>
+        <th class="w-5"></th>`;
+let tr = `
+        <td><a href="<?= getIndex() ?>bar" class="text-decoration-none btn btn-light btn-outline-secondary">1</a></td>
+        <td class="w-10"><input class="w-100 form-control" type="text" name="nombre" id="nombre" value="Edelweiss"></td>
+        <td class="w-50"><input class="w-100 form-control" type="text" name="direccion" id="direccion" value="Avenida de la playa, 3 bajo, 26009, Logroño, La Rioja"></td>
+        <td><input class="mx-auto mt-2 form-check form-check-input" type="checkbox" name="terraza" id="terraza" checked></td>
+        <td><input class="w-100 form-control" type="number" name="latitud" id="latitud" step="0.0001" value="43.2642"></td>
+        <td><input class="w-100 form-control" type="number" name="latitud" id="latitud" step="0.0001" value="25.3243"></td>
+        <td>
+            <button class="btn btn-danger" title="Eliminar">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                </svg>
+            </button>
+        </td>`;
+
 function mostrarDatos() {
-    ajax.loadContent("index.php/bares?cantidad=" + cantidad + "&pagina=" + pagina, "GET", () => {
+    ajax.loadContent("index.php/bares?cantidad=" + cantidad + "&pag=" + pag, "GET", () => {
         bares = eval(ajax.getResponse());
 
-        var numFilas = document.getElementById("selectFilas").value;
-        if (numFilas == "") {
-            document.getElementById("tabla").innerHTML = "";
+        if (cantidad == 0) {
+            /*
+                VACIAR HTML DE TRS
+            */
             return;
         } else {
-            numFilas = parseInt(numFilas);
-            let indice = document.getElementById("from").value;
             //  Si se cambia la cantidad de registros a mostrar
-            if (numFilas !== num_filas) {
+            if (cantidad !== registros) {
+                pag = 0;
                 pagina = 0;
-                indice = 0;
             }
-            num_filas = numFilas;
-            document.getElementById("from").value = indice;
-            //  Pedimos un registro de mas, si nos devuelve la cantidad pedida, es que todavia hay registros, si no, deshabilitamos el boton de siguiente
-            ajax.loadContent("getEmpleados.php?q=" + (numFilas + 1) + "&from=" + indice + "&order_by=" + order_by + "&asc=" + order_by_asc, "GET", () => {
-                var resultados = eval(ajax.getResponse());
-                let resLength = resultados.length;
-                let siguienteDeshabilitado = true;
-                if (resLength >= numFilas + 1) {
-                    siguienteDeshabilitado = false;
-                    --resLength;
+            registros = cantidad;
+            /*
+                ESTABLECER VALOR EN DESDE (usando pagina) PARA PAGINACION
+            */
+            let resLength = bares.length;
+            let siguienteDeshabilitado = true;
+            if (resLength >= cantidad + 1) {
+                siguienteDeshabilitado = false;
+                --resLength;
+            }
+            document.getElementsByName(/* ID DE BOTON SIGUIENTE */)[0].disabled = siguienteDeshabilitado;
+            document.getElementsByName(/* ID DE BOTON ANTERIOR */)[0].disabled = pagina == 1;
+            //  Cambiar contenido de tabla
+            if (resLength > 0) {
+                let tabla = document.getElementById(/* ID DE TABLA */);
+                let thead = tabla.getElementsByTagName("thead")[0];
+                let tbody = tabla.getElementsByTagName("tbody")[0];
+                construirCabecera(thead);
+                for (let i = 0; i < resLength; i++) {
+                    let fila = construirFila(bares[i], i);
+                    tbody.appendChild(fila);
                 }
-                document.getElementsByName("siguiente")[0].disabled = siguienteDeshabilitado;
-                document.getElementsByName("anterior")[0].disabled = indice == 0;
-                //  Crear tabla
-                if (resLength > 0) {
-                    var tabla = document.createElement('table');
-                    tabla.setAttribute("border", 1);
-                    var cabecera = construirCabecera();
-                    tabla.id = "tablaEmpleados";
-                    tabla.appendChild(cabecera);
-                    for (let i = 0; i < resLength; i++) {
-                        let fila = construirFila(resultados[i], i);
-                        tabla.appendChild(fila);
-                    }
-                    document.getElementById("tabla").innerHTML = "";
-                    document.getElementById("tabla").appendChild(tabla);
-                    //  Agregar flecha de ordenacion en campo de cabecera
-                    document.getElementsByTagName('th')[order_by].textContent += order_by_asc ? "↓" : "↑";
-                }
-            });
+                //  Agregar flecha de ordenacion en campo de cabecera
+                // document.getElementsByTagName('th')[order_by].textContent += order_by_asc ? "↓" : "↑";
+            }
         }
     });
 }
 
 
 function siguiente() {
-    document.getElementById("from").value = num_filas * ++pagina;
+    /* ESTABLECER VALOR DESDE
+    document.getElementById("from").value = registros * ++pag;
+    */
     mostrarDatos();
 }
 
 function anterior() {
-    document.getElementById('from').value = num_filas * --pagina;
+    /* ESTABLECER VALOR DESDE
+    document.getElementById('from').value = registros * --pag;
+    */
     mostrarDatos();
 }
 
-function ordenar(indice) {
-    if (order_by != indice) {
+function ordenar(pagina) {
+    if (order_by != pagina) {
         order_by_asc = 1;
-        order_by = indice;
+        order_by = pagina;
     } else {
         order_by_asc = order_by_asc == 1 ? 0 : 1;
     }
@@ -123,7 +146,7 @@ function insertarEmpleado() {
         fields[7].parentElement.appendChild(mensajeError);
     } else {
         user = JSON.stringify(user);
-        ajax.loadContent("insertarEmpleado.php?user=" + user, "GET", () => {});
+        ajax.loadContent("insertarEmpleado.php?user=" + user, "GET", () => { });
         limpiarInsertarEmpleado();
     }
 }
@@ -165,114 +188,40 @@ function actualizarFila(n) {
         fields[7].parentElement.appendChild(mensajeError);
     } else {
         user = JSON.stringify(user);
-        ajax.loadContent("actualizarEmpleado.php?user=" + user, "GET", () => {});
+        ajax.loadContent("actualizarEmpleado.php?user=" + user, "GET", () => { });
     }
 }
 
 function construirCabecera() {
     var cabecera = document.createElement('tr');
-
-    var titulo = document.createElement('th');
-    var texto = document.createTextNode("Núm. Empleado");
-    titulo.appendChild(texto);
-    titulo.style.cursor = "pointer";
-    var funcion = function() {
-        ordenar(0);
-    };
-    titulo.onclick = funcion;
-    cabecera.appendChild(titulo);
-
-    var titulo = document.createElement('th');
-    var texto = document.createTextNode("Apellido");
-    titulo.style.cursor = "pointer";
-    var funcion = function() {
-        ordenar(1);
-    };
-    titulo.onclick = funcion;
-    titulo.appendChild(texto);
-    cabecera.appendChild(titulo);
-
-    var titulo = document.createElement('th');
-    var texto = document.createTextNode("Oficio");
-    titulo.style.cursor = "pointer";
-    var funcion = function() {
-        ordenar(2);
-    };
-    titulo.onclick = funcion;
-    titulo.appendChild(texto);
-    cabecera.appendChild(titulo);
-
-    var titulo = document.createElement('th');
-    var texto = document.createTextNode("Director");
-    titulo.style.cursor = "pointer";
-    var funcion = function() {
-        ordenar(3);
-    };
-    titulo.onclick = funcion;
-    titulo.appendChild(texto);
-    cabecera.appendChild(titulo);
-
-    var titulo = document.createElement('th');
-    var texto = document.createTextNode("Fecha Alta");
-    titulo.style.cursor = "pointer";
-    var funcion = function() {
-        ordenar(4);
-    };
-    titulo.onclick = funcion;
-    titulo.appendChild(texto);
-    cabecera.appendChild(titulo);
-
-    var titulo = document.createElement('th');
-    var texto = document.createTextNode("Salario");
-    titulo.style.cursor = "pointer";
-    var funcion = function() {
-        ordenar(5);
-    };
-    titulo.onclick = funcion;
-    titulo.appendChild(texto);
-    cabecera.appendChild(titulo);
-
-    var titulo = document.createElement('th');
-    var texto = document.createTextNode("Comisión");
-    titulo.style.cursor = "pointer";
-    var funcion = function() {
-        ordenar(6);
-    };
-    titulo.onclick = funcion;
-    titulo.appendChild(texto);
-    cabecera.appendChild(titulo);
-
-    var titulo = document.createElement('th');
-    var texto = document.createTextNode("Departamento");
-    titulo.style.cursor = "pointer";
-    var funcion = function() {
-        ordenar(7);
-    };
-    titulo.onclick = funcion;
-    titulo.appendChild(texto);
-    cabecera.appendChild(titulo);
-
+    cabecera.innerHTML = ths;
     return cabecera;
 }
 
 function construirFila(datos, n) {
-    linea = document.createElement('tr');
+    let fila = document.createElement('tr');
+    fila.innerHTML = tr;
+    let columnas = fila.querySelectorAll('td');
 
-    var titulo = document.createElement('td');
-    var campo = document.createElement('input');
-    campo.className = "emp_no";
-    campo.type = "number";
-    campo.value = datos.emp_no;
-    campo.setAttribute("readonly", true);
-    titulo.appendChild(campo);
-    linea.appendChild(titulo);
+    columnas[0].querySelector('a').innerText = datos.id;
+    columnas[1].querySelector('a').innerText = datos.id;
+    columnas[2].querySelector('a').innerText = datos.id;
+    columnas[3].querySelector('a').innerText = datos.id;
+    columnas[4].querySelector('a').innerText = datos.id;
+    columnas[5].querySelector('a').innerText = datos.id;
+    columnas[0].querySelector('a').innerText = datos.id;
+
+    for (let i = 0; i < columnas.length; i++) {
+        const columna = columnas[i];
+        columna.onblur = function () { actualizarFila(n) };
+    }
 
     var titulo = document.createElement('td');
     var campo = document.createElement('input');
     campo.className = "apellido";
     campo.type = "text";
     campo.value = datos.apellido;
-    campo.onblur = function() { actualizarFila(n) };
+    campo.onblur = function () { actualizarFila(n) };
     titulo.appendChild(campo);
     linea.appendChild(titulo);
 
@@ -281,7 +230,7 @@ function construirFila(datos, n) {
     campo.className = "oficio";
     campo.type = "text";
     campo.value = datos.oficio;
-    campo.onblur = function() { actualizarFila(n) };
+    campo.onblur = function () { actualizarFila(n) };
     titulo.appendChild(campo);
     linea.appendChild(titulo);
 
@@ -290,7 +239,7 @@ function construirFila(datos, n) {
     campo.className = "dir";
     campo.type = "number";
     campo.value = datos.dir;
-    campo.onblur = function() { actualizarFila(n) };
+    campo.onblur = function () { actualizarFila(n) };
     titulo.appendChild(campo);
     linea.appendChild(titulo);
 
@@ -299,7 +248,7 @@ function construirFila(datos, n) {
     campo.className = "fecha_alt";
     campo.type = "text";
     campo.value = datos.fecha_alt;
-    campo.onblur = function() { actualizarFila(n) };
+    campo.onblur = function () { actualizarFila(n) };
     titulo.appendChild(campo);
     linea.appendChild(titulo);
 
@@ -308,7 +257,7 @@ function construirFila(datos, n) {
     campo.className = "salario";
     campo.type = "number";
     campo.value = datos.salario;
-    campo.onblur = function() { actualizarFila(n) };
+    campo.onblur = function () { actualizarFila(n) };
     titulo.appendChild(campo);
     linea.appendChild(titulo);
 
@@ -317,7 +266,7 @@ function construirFila(datos, n) {
     campo.className = "comision";
     campo.type = "number";
     campo.value = datos.comision;
-    campo.onblur = function() { actualizarFila(n) };
+    campo.onblur = function () { actualizarFila(n) };
     titulo.appendChild(campo);
     linea.appendChild(titulo);
 
@@ -335,7 +284,7 @@ function construirFila(datos, n) {
         }
         campo.add(option, null);
     });
-    campo.onblur = function() { actualizarFila(n) };
+    campo.onblur = function () { actualizarFila(n) };
     titulo.appendChild(campo);
     linea.appendChild(titulo);
 
